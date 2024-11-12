6e72516e2260195330dff9227759bcb8ecbfa5bb
Moses Valenzuela <developinghephepaestus@gmail.com>
2024-11-12 08:26:34 -0800

    cleaned up html form and began adding the form validation to the css files

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const frmElement = document.getElementById("myform");
+const frmLastName = document.getElementById("frmLastName");
+const frmEmail = document.getElementById("frmEmail");
+const frmPass = document.getElementById("frmPass");
 const form = document.querySelector("form");
-const frmFields = document.getElementsByClassName("input-form");
+const frmName = document.getElementById("frmName");
 
+console.log(frmEmail);
+
+frmElement.addEventListener("submit", event=>{
+
+    event.preventDefault();
+    console.log("hello");
+    
+});
